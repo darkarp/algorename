b5719e5cf8024c5fa390152a193f10fa2f04bfd8
Mário Nascimento <mario@whitehathacking.tech>
2023-09-06 03:31:25 +0100

    fix multiprocessing logs

--- run.py
+++ run.py
@@ -6,9 +6,17 @@ import logging
 import json
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
+from concurrent.futures import ProcessPoolExecutor
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Setting verbosity levels and log file name
+VERBOSITY = os.getenv("FCLOG_LEVEL", "INFO").upper()
+LOG_FILENAME = os.getenv("FCLOG_NAME", "file_change.log").lower()
 
 
-def setup_logging(verbosity_level: str, filename: str = "file_changer.log", max_size: int = (1024 ** 2) * 1):
+def setup_logging(verbosity_level: str, filename: str = LOG_FILENAME, max_size: int = (1024 ** 2) * 1):
     """Setting up logging
 
     Args:
@@ -16,7 +24,7 @@ def setup_logging(verbosity_level: str, filename: str = "file_changer.log", max_
     """
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
     log_handler = RotatingFileHandler(filename, maxBytes=max_size, backupCount=5)
-    logging.basicConfig(handlers=[log_handler], format=log_format, level=getattr(logging, verbosity_level, "INFO"))
+    logging.basicConfig(handlers=[log_handler], format=log_format, level=getattr(logging, verbosity_level, VERBOSITY))
 
 
 def log_json(action: str, old: str, new: str):
@@ -50,18 +58,32 @@ def shift_filename(filename: str) -> str:
     return shifted_name
 
 
+def shift_file_task(folder_path: str, filename: str):
+    """Task to rename a single file by shifting its name to the left by 1.
+
+    Args:
+        folder_path (str): The path to the folder containing the file.
+        filename (str): The filename to be shifted.
+    """
+
+    setup_logging(VERBOSITY, filename=LOG_FILENAME, max_size=(1024 ** 2) * 1)
+
+    original_path = os.path.join(folder_path, filename)
+    new_filename = shift_filename(filename)
+    new_path = os.path.join(folder_path, new_filename)
+    os.rename(original_path, new_path)
+    log_json("Shifted Directory File", original_path, new_path)
+
+
 def shift_directory(folder_path: str):
     """Rename all files in the given directory by shifting their names to the left by 1.
 
     Args:
         folder_path (str): Path to the directory whose filenames need to be shifted.
     """
-    for filename in os.listdir(folder_path):
-        original_path = os.path.join(folder_path, filename)
-        new_filename = shift_filename(filename)
-        new_path = os.path.join(folder_path, new_filename)
-        os.rename(original_path, new_path)
-        log_json("Shifted Directory File", original_path, new_path)
+    filenames = os.listdir(folder_path)
+    with ProcessPoolExecutor() as executor:
+        executor.map(shift_file_task, [folder_path] * len(filenames), filenames)
 
 
 def shift_single_file(file_path: str):
@@ -77,13 +99,11 @@ def shift_single_file(file_path: str):
     log_json("Shifted Single File", file_path, new_path)
 
 
-def main():
+def main(verbosity_level: str = VERBOSITY, log_filename: str = LOG_FILENAME):
     """Main function.
 
     It sets up the logging, parses CLI arguments, and calls the appropriate function to rename files or directories.
     """
-    # Load environment variables from .env file
-    load_dotenv()
 
     parser = argparse.ArgumentParser(description="Shift file or directory names by 1 character to the left.")
     parser.add_argument("-s", "--silent", help="Suppress errors.", action="store_true")
@@ -94,11 +114,8 @@ def main():
 
     args = parser.parse_args()
 
-    # Setting verbosity levels and logging after parsing arguments
-    verbosity_level = os.getenv("FCLOG_LEVEL", "INFO").upper()
-    log_filename = os.getenv("FCLOG_NAME", "file_changer.log").lower()
-
-    setup_logging(verbosity_level, log_filename, max_size=(1024**2)*1)
+    # Setting up logging after parsing the arguments
+    setup_logging(verbosity_level, filename=log_filename, max_size=(1024 ** 2) * 1)
 
     try:
         if args.file:
